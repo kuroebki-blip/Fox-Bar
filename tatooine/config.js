3315b9a7134cfee5dc15aed3c556990bb4d929de
Захар Захарченко <woodie3319@MacBook-Air-Zahar.local>
2026-07-22 18:31:11 +0300

    fix: preserve Tatooine file selection v1.1.1

--- tatooine/config.js
+++ tatooine/config.js
@@ -1,5 +1,5 @@
 window.TATOOINE_CONFIG = Object.freeze({
-  version: 'v1.1.0 PETROVKA REPORT TEMPLATE',
+  version: 'v1.1.1 FILE INPUT FIX',
   apiUrl: 'https://script.google.com/macros/s/AKfycbx9XlQG6kCvWVU6OekcWAmHAVnjFXfG-_UD_pKQrQqYaWNyHzmXsmB_2LGohxETrrfTpA/exec',
   venue: 'tatooine',
   restaurantName: 'Tatooine',
